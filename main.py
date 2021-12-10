9042fec7f7869205c1fc1fca7a9225435d7ffaac
Steven Vandegrift <svandegrift@icloud.com>
2021-12-10 14:43:22 -0600

    Fix ASCII

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 import random
+import hangman_words
+import hangman_art
 
-#1: Update the word list to use the 'word_list' from hangman_words.py
-#Delete this line: word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
-#3: - Import the logo from hangman_art.py and print it at the start of the game.
-
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+print(hangman_art.logo)
 
 #Create blanks
 display = []
@@ -22,18 +19,18 @@ for _ in range(word_length):
 while not end_of_game:
 	guess = input("Guess a letter: ").lower()
 	
-	#4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-	
+	if guess in display:
+		print(f"You have already guessed {guess}. Try again")
+		
 	#Check guessed letter
 	for position in range(word_length):
-		letter = chosen_word[position]
-		print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+		letter = chosen_word[position]		
 		if letter == guess:
 			display[position] = letter
 			
 	#Check if user is wrong.
 	if guess not in chosen_word:
-		#TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+		print(f"{guess} is not in the word.")
 		lives -= 1
 		if lives == 0:
 			end_of_game = True
@@ -46,6 +43,5 @@ while not end_of_game:
 	if "_" not in display:
 		end_of_game = True
 		print("You win.")
-		
-	#2: - Import the stages from hangman_art.py and make this error go away.
-	print(stages[lives])
+			
+	print(hangman_art.stages[lives])
