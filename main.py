cf95dae06aded0e11baac1d03d3a8aa62bfde7da
Steven Vandegrift <svandegrift@icloud.com>
2021-12-12 23:02:12 -0600

    Improve UI
    Now when running in the terminal, program does not keeps adding onto itself but rather clears screen to make experience clean

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import random
 import hangman_words
 import hangman_art
+import os
+
+def clear_console():
+	os.system('clear')
 
 chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
@@ -9,14 +13,21 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-print(hangman_art.logo)
+
 
 #Create blanks
 display = []
 for _ in range(word_length):
 	display += "_"
-	
+
+wrong_guesses = []
+
 while not end_of_game:
+	clear_console()
+	print(hangman_art.logo)
+	print(f"Wrong: {wrong_guesses}")
+	print(hangman_art.stages[lives])
+	print(f"{' '.join(display)}")
 	guess = input("Guess a letter: ").lower()
 	
 	if guess in display:
@@ -30,18 +41,20 @@ while not end_of_game:
 			
 	#Check if user is wrong.
 	if guess not in chosen_word:
-		print(f"{guess} is not in the word.")
+		wrong_guesses.append(guess)
 		lives -= 1
 		if lives == 0:
 			end_of_game = True
+			clear_console()
+			print(hangman_art.logo)
+			print(f"Wrong: {wrong_guesses}")
+			print(hangman_art.stages[lives])
+			print(f"{' '.join(display)}")			
 			print("You lose.")
-			
-	#Join all the elements in the list and turn it into a String.
-	print(f"{' '.join(display)}")
 	
 	#Check if user has got all letters.
 	if "_" not in display:
 		end_of_game = True
 		print("You win.")
 			
-	print(hangman_art.stages[lives])
+	
